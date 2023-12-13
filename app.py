f6e0082c025f16f89a845e35b841abfb8503528f
ormesh90 <153753802+ormesh90@users.noreply.github.com>
2023-12-13 16:24:39 +0000

    Reverted back to gpt 3

--- app.py
+++ app.py
@@ -214,7 +214,7 @@ def load_model():
     # Get the OpenAI Chat Model
     return ChatOpenAI(
         temperature=0.3,
-        model='gpt-4-1106-preview',
+        model='gpt-3.5-turbo',
         streaming=True,
         verbose=True
     )
